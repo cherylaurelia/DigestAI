f2eeb4ff792db90dd3d95e95d78439e21db79c2c
cheryl <135518448+cherylaurelia@users.noreply.github.com>
2024-03-23 20:35:35 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const express = require("express")
+const OpenAI = require("openai")
+const app = express()
+app.use(express.json())
 
+const openai = new OpenAI({
+    apiKey:""
+})
+
+app.get('/getResponse',async(req,res)=>{
+
+    const response = await openai.chat.completions.create({
+        model:'gpt-4.0',
+        messages: [{"role":"user","content":"essay on global warming"}],
+        max_tokens:100,
+        temperature : 0.7,
+    })
+    console.log(response)
+})
 
 app.listen(3000,()=>{
     console.log("server started")
